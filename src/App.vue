7db9cf064c5402b477244bdea8ab2d34aeda94e3
songjun51 <song@wonjun.kr>
2018-03-26 14:22:42 +0900

    main

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <div id="background-alpah">
+    <div id="main">
+      <div id="timerview">
+      <div id="timerstartmsg" class="font timer">{{timerstartmsg}}</div>
+      <div id="timer" class="font timer">12<span>시간</span> 23<span>분</span> 34<span>초</span></div>
+      <div id="timerendmsg" class="font timer">{{timerendmsg}}</div>
+      </div>
+      <div id="timetable">asdf
+        <div id="timeline"></div>
+        <div id="timemsg"></div>
+      </div>
+      <div id="nav">
+        <div id="nav1" class="navitem">
+          <a href="#">
+         <div class="iconarea"> <img id="icon1" src="./assets/page-1@3x.png"></div>
+          <div class="navmsg font">주제 확인</div></a>
+        </div>
+
+
+         <div id="nav2" class="navitem">
+           <a href="#">
+          <div class="iconarea"><img id="icon2" src="./assets/page-1_2@3x.png"></div>
+          <div class="navmsg font">이벤트 & 규칙</div></a>
+        </div>
+
+         <div id="nav3" class="navitem">
+           <a href="#">
+          <div class="iconarea"><img id="icon3" src="./assets/page-1_3@3x.png"></div>
+          <div class="navmsg font">결과물 제출 </div></a>
+        </div>
+
+         <div id="nav4" class="navitem" >
+           <a href="#">
+          <div class="iconarea"><img id="icon4" src="./assets/page-1_4@3x.png"></div>
+          <div class="navmsg font">서비스 조사</div></a>
+        </div>
+
+
+         <div id="nav5" class="navitem" v-mouseover="up" @mouseleave="up" >
+           <a href="https://www.facebook.com/smarteenappclub/" target="_blank">
+         <div class="iconarea"> <img  id="icon5" src="./assets/page-1_5@3x.png"></div>
+          <div class="navmsg font" >페이스북 페이지</div></a>
+        </div>
+      </div>
+
+
+
+
+    </div>
+
+
+
+
+
+</div>
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: "#app",
+  data() {
+    //데이터는 정적인 데이터 처리해
+    return {
+      timerstartmsg: "15회 앱잼은 현재",
+      timerendmsg: "남았습니다",
+      up : false
+    };
+  },
+  computed: {
+    // 계산 필요한 동적 데이터 처리
+  },
+  methods: {
+    // 메소드 ()
+  },
+  watch: {
+    // 뒷편에서 이벤트 잡을려고
+  }
+};
+//나누는 이유는 뷰 규칙, 데이터랑 컴퓨트랑 나누는 이유는 연산 등 내부 로직자체가 달라서 퍼포먼스때문에
+</script>
+
+<style>
+/* @import "assets/app.css"; */
+</style>
